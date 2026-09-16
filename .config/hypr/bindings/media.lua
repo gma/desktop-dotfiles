9ec55c16724cb15831e32e89d96888aba6c6b8a8
Graham Ashton <graham@effectif.com>
2026-09-16 08:11:41 +0100

    Change audio volume in 2% increments

--- .config/hypr/bindings/media.lua
+++ .config/hypr/bindings/media.lua
@@ -1,10 +1,10 @@
 hl.bind("XF86AudioRaiseVolume",
-  hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"),
+  hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 2%+"),
   { locked = true, repeating = true }
 )
 hl.bind(
   "XF86AudioLowerVolume",
-  hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"),
+  hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 2%-"),
   { locked = true, repeating = true }
 )
 hl.bind(
